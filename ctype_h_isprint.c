351b4a74db51483f4da38924191e57adbade64fb
rameshbalakh <melwinmania@gmail.com>
2024-05-03 22:45:10 +0530

    isprint function

---
+++ ctype_h_isprint.c
@@ -0,0 +1,37 @@
+// check given input printable or not
+/*
+* output
+* 8 is printable
+* H is printable
+*      is not printable
+*  is not printable
+*/
+#include <stdio.h>
+#include <ctype.h>
+int main(){
+    int var1 = '8';
+    int var2 = 'H';
+    int var3 = '\t';
+    int var4 = ' ';
+    
+    if(isprint(var1)){
+        printf("%c is printable\n",var1);
+    } else {
+        printf("%c is not printable\n",var1);
+    }
+    if(isprint(var2)){
+        printf("%c is printable\n",var2);
+    } else {
+        printf("%c is not printable\n",var2);
+    }
+    if(isprint(var3)){
+        printf("%c is printable\n",var3);
+    } else {
+        printf("%c is not printable\n",var3);
+    }
+    if(isprint(var3)){
+        printf("%c is printable\n",var4);
+    } else {
+        printf("%c is not printable\n",var4);
+    }
+}
