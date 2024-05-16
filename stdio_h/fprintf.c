b0445d2f4e36f9f8c417440e2019792acb56b84b
rameshbalakh <melwinmania@gmail.com>
2024-05-16 21:27:21 +0530

    fprintf function using small code

---
+++ stdio_h/fprintf.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+
+    // create a file pointer to write or read
+    FILE *fptr;
+    // sample data 
+    char student_2[]="kamalakasan";
+    char student_3[]="M.G.Ramachandhiran";
+    char student_4[]="Sivaji ganesan";
+    fptr = fopen("sample.txt","w");
+
+    // check the file whether or not
+    if(fptr == NULL){
+        printf("Error to open a file !");
+        return 0;
+    }
+    
+    // write a data to the file one bye one
+    fprintf(fptr,"student no 1: %s\n","Rajinikanth");
+    fprintf(fptr,"student no 2: %s\n",student_2);
+    fprintf(fptr,"student no 3: %s\n",student_3);
+    fprintf(fptr,"student no 4: %s\n",student_4);
+
+    // small conformation to written
+    printf("file written successfully");
+}
