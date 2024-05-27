d8405285013dc7cf46021d9ff4eec22075678122
rameshbalakh <melwinmania@gmail.com>
2024-05-27 23:48:15 +0530

    fread in c function

---
+++ stdio_h/fread.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main () 
+{
+    // buffer to store data
+    char buffer[600];
+    
+    // FILE object
+    FILE * file;
+    // opening a file in read mode
+    file = fopen("sample.txt", "r");
+    
+    int count = fread(&buffer, sizeof(char), 600, file);
+    
+    // close opened file
+    fclose(file);
+    
+    printf("Data read from the file:\n%s \n", buffer);
+   
+    return 0;
+}
