1cc1bd5830463eeec287afed62814db15d4ebca4
rameshbalakh <melwinmania@gmail.com>
2024-06-09 23:13:38 +0530

    freopen function

--- stdio_h/freopen.c
+++ stdio_h/freopen.c
@@ -2,13 +2,16 @@
 int main(){
     FILE *ptr;
 
-
+    // sample display for user
     printf("This text is redirecting to stdout\n");
 
+    // reopen the file using stdout redirecting
     ptr = freopen("freopen.txt","w+", stdout);
 
+    //This text for print the file
     printf("This is sample text for freopen",ptr);
 
+    // close the ptr
     fclose(ptr);
 
     return 0;
