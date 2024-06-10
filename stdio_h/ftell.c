e6f956f0c56061f2cb50eb1c968dbcd80bbbd38b
rameshbalakh <melwinmania@gmail.com>
2024-06-10 20:30:24 +0530

    written a explainaiton

--- stdio_h/ftell.c
+++ stdio_h/ftell.c
@@ -10,12 +10,16 @@ int main(){
         return (-1);
     }
 
+    // ptr start with zero
     fseek(ptr, 0, SEEK_END);
 
+    // ftell function store the current ptr position
     len = ftell(ptr);
 
+    // close the file
     fclose(ptr);
 
+    // display the output to the user
     printf("Total size of sample file: %d",len);
 
     return 0;
